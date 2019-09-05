5f1b91086c043126623aca31a9d472dcec9df950
nmunshi <nmunshi@cisco.com>
2019-09-06 00:53:42 +0530

    rookie mistake

--- src/main/java/com/chatx/back/controller/LoginController.java
+++ src/main/java/com/chatx/back/controller/LoginController.java
@@ -4,11 +4,13 @@ package com.chatx.back.controller;
 import com.chatx.back.model.User;
 import com.chatx.back.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class LoginController {
 
     @Autowired
